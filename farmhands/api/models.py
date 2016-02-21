d271dc6c7b0a44b3ddc2f3280e3a4a187acd3bc1
ladeo <olufunmilade.oshodi@andela.com>
2016-02-21 19:40:33 +0100

    [Fix] Updated the models

--- farmhands/api/models.py
+++ farmhands/api/models.py
@@ -5,8 +5,50 @@ Models for farmhands api
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Crop(models.Model):
-    pass
+
+    CLIMATE = (
+        ('W', 'Tropical Wet'),
+        ('MO', 'Monsoon'),
+        ('S', 'Savanna'),
+        ('A', 'Arid'),
+        ('SA', 'Semi Arid'),
+        ('ME', 'Mediterranean'),
+        ('HS', 'Humid Subtropical'),
+        ('MA', 'Marine'),
+        ('WS', 'Warm Summer'),
+        ('CS', 'Cool Summer'),
+        ('B', 'Boreal'),
+        ('T', 'Tundra'),
+        ('I', 'Ice cap'),
+    )
+
+    LIFE_CYCLE = (
+        ('ANNUAL', 'Annual'),
+        ('BIENNAL', 'Biennal'),
+        ('PERENNIAL', 'Perennial'),
+        ('EPHEMERALS', 'Ephemerals'),
+    )
+
+    CROP_CATEGORIES = (
+        ('FOOD', 'Food'),
+        ('FEED', 'Feed'),
+        ('FIBRE', 'Fibre'),
+        ('OIL', 'Oil'),
+        ('ORNAMENTAL', 'Ornamental'),
+        ('INDUSTRIAL', 'Industrial'),
+    )
+
+    name = models.CharField(max_length=200)
+    climate = models.CharField(max_length=30, choices=CLIMATE)
+    crop_categories = models.CharField(max_length=30, choices=CROP_CATEGORIES)
+    life_cycle = models.CharField(max_length=30, choices=LIFE_CYCLE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    desc = models.CharField(max_length=200)
+    date_created = models.DateTimeField(
+        auto_now_add=True, blank=True, verbose_name='created')
+
 
 class Plan(models.Model):
 
@@ -17,9 +59,8 @@ class Plan(models.Model):
     )
 
     name = models.CharField(max_length=200)
+    crop = models.ForeignKey(Crop, related_name='plan')
     farm_size = models.CharField(max_length=30, choices=FARM_SIZES)
-    weather = models.CharField(max_length=200)
-    crop_type = models.CharField(max_length=200)
     budget = models.DecimalField(max_digits=8, decimal_places=2)
     duration = models.IntegerField(default=0)
     date_created = models.DateTimeField(
--- farmhands/api/serializers.py
+++ farmhands/api/serializers.py
@@ -3,7 +3,7 @@ Serializers for farmhands
 """
 
 from rest_framework import serializers
-from api.models import Plan, Schedule
+from api.models import Plan, Schedule, Crop
 from django.contrib.auth.models import User
 
 
@@ -19,14 +19,31 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
 
+class CropSerializer(serializers.ModelSerializer):
+    plan = serializers.ReadOnlyField(source='plan.crop')
+
+    class Meta:
+        model = Crop
+        fields = (
+            'name', 'climate', 'crop_categories', 'life_cycle',
+            'price', 'desc', 'date_created',
+            'plan'
+        )
+
+
 class PlanSerializer(serializers.ModelSerializer):
     users = UserSerializer(many=True)
     schedule = serializers.ReadOnlyField(source='schedule.plan')
+    crop = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
 
     class Meta:
         model = Plan
         fields = (
-            'name', 'farm_size', 'weather', 'crop_type', 'budget',
+            'name', 'farm_size', 'crop', 'budget',
             'duration', 'date_created', 'users',
             'schedule'
         )
