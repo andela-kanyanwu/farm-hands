296db5945ad5976290134eb0c92064fd45dedb77
andela-ashuaib <abiodun.shuaib@andela.com>
2016-02-22 04:50:09 +0100

    (chore) add redirect

--- ngclient/src/js/controllers/authctrl.js
+++ ngclient/src/js/controllers/authctrl.js
@@ -1,12 +1,13 @@
 module.exports = authCtrl;
-function authCtrl($log, google, $rootScope) {
+function authCtrl($log, google, $rootScope, $state) {
   var vm = this;
   vm.googleLogin = function(){
     google.login(function(res){
         $rootScope.currentUser = res;
+        $state.go('index');
     })
   }
   vm.test = "Testing...";
   $log.debug(vm.test, 'auth');
 }
-authCtrl.$inject = ['$log', 'google', '$rootScope'];
+authCtrl.$inject = ['$log', 'google', '$rootScope', '$state'];
